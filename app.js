0690479f90af1e40d4f8ce8544267a14182c1049
GHSoyad <ghsoyad@gmail.com>
2022-09-03 00:03:46 +0800

    News container and div created

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const loadCategories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    displayNews(data.data.news_category);
+    displayCategoryList(data.data.news_category);
 }
 
-const displayNews = categories => {
+const displayCategoryList = categories => {
     const categoryNav = document.getElementById('category-nav');
     categories.forEach(category => {
         // console.log(category)
@@ -26,4 +26,35 @@ const loadNews = async (categoryId) => {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data.data);
+    displayNews(data.data);
+}
+
+const displayNews = news => {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
+
+    news.forEach(eachNews => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+
+        newsDiv.innerHTML = `
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${eachNews.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${eachNews.title}</h5>
+                        <p class="card-text">${eachNews.details.slice(0, 180)}...</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        newsContainer.appendChild(newsDiv);
+    })
+
+
 }
