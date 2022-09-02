2579c1cbb3fad47ea25b234b87d07be0c1f59faf
GHSoyad <ghsoyad@gmail.com>
2022-09-02 23:11:03 +0800

    Categories data fetched

---
+++ app.js
@@ -0,0 +1,8 @@
+const loadCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data)
+}
+
+loadCategories();
