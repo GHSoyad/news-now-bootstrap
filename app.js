a5577cc50dcd77217f8258ba30e9099e90063cf4
GHSoyad <ghsoyad@gmail.com>
2022-09-02 23:26:39 +0800

    Category nav created

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const loadCategories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data)
+    displayNews(data.data.news_category);
+}
+
+const displayNews = categories => {
+    const categoryNav = document.getElementById('category-nav');
+    categories.forEach(category => {
+        console.log(category)
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML = `
+        <a class="nav-link">${category.category_name}</a>
+        `
+        categoryNav.appendChild(li);
+    });
 }
 
 loadCategories();
