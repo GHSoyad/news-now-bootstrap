57b5f44878d10833a5be9a2103fb363aef71c04f
GHSoyad <ghsoyad@gmail.com>
2022-09-03 08:21:59 +0800

    Added active category style, sorting section, animated modal button and some minor changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Function to fetch category list
 const loadCategories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     try {
@@ -9,13 +10,14 @@ const loadCategories = async () => {
     }
 }
 
+// Function to display category list
 const displayCategoryList = categories => {
     const categoryNav = document.getElementById('category-nav');
     categories.forEach(category => {
         const li = document.createElement('li');
         li.classList.add('nav-item');
         li.innerHTML = `
-        <a onclick="loadNews('${category.category_id}', '${category.category_name}')" class="nav-link">${category.category_name}</a>
+        <a onclick="loadNews('${category.category_id}', '${category.category_name}')" class="nav-link category-link">${category.category_name}</a>
         `
         categoryNav.appendChild(li);
     });
@@ -23,6 +25,7 @@ const displayCategoryList = categories => {
 
 loadCategories();
 
+// Function to fetch news list
 const loadNews = async (categoryId, categoryName) => {
     displayLoader(true);
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
@@ -35,22 +38,30 @@ const loadNews = async (categoryId, categoryName) => {
     }
 }
 
+// Function to display news list
 const displayNews = (news, categoryName) => {
+    // Clear container
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
 
+    // Number of news found message
+    const newsFound = document.getElementById('news-found');
+    if (news.length !== 0) {
+        newsFound.innerHTML = `${news.length} news found in category <span class="text-primary">${categoryName}</span>`
+    } else {
+        newsFound.innerHTML = `No news found in category <span class="text-primary">${categoryName}</span>`
+    }
+
+    // No news found message
     const noNewsFound = document.getElementById('no-news-found-message');
     if (news.length === 0) {
         noNewsFound.classList.remove('d-none');
-        noNewsFound.innerHTML = `<p>No News Found in ${categoryName}</p>`
+        noNewsFound.innerHTML = `<p>No News Found in Category ${categoryName}</p>`
     } else {
         noNewsFound.classList.add('d-none');
     }
 
-    const newsFound = document.getElementById('news-found');
-    newsFound.innerHTML = `${news.length} news found in category <span class="text-primary">${categoryName}</span>`
-    newsFound.classList.remove('d-none');
-
+    // Sort news list by views
     news = news.sort((a, b) => b.total_view - a.total_view);
 
     news.forEach(eachNews => {
@@ -59,14 +70,14 @@ const displayNews = (news, categoryName) => {
 
         newsDiv.innerHTML = `
         <div class="card bg-white h-100">
-            <div class="row g-0">
+            <div class="row g-0 h-100">
                 <div class="col-md-4">
                     <img src="${eachNews.thumbnail_url}" class="img-fluid rounded w-100 h-100" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body h-100 d-flex flex-column justify-content-between">
-                        <h5 class="card-title">${eachNews.title}</h5>
-                        <p class="card-text">${eachNews.details.slice(0, 180)}...</p>
+                        <h5 class="card-title fw-bold">${eachNews.title}</h5>
+                        <p class="card-text">${eachNews.details.slice(0, 150)}...</p>
                         <div class="d-flex justify-content-between align-items-center flex-wrap gap-3">
                             <div class="author-info d-flex">
                                 <div>
@@ -95,6 +106,7 @@ const displayNews = (news, categoryName) => {
     displayLoader(false);
 }
 
+// Function to fetch news details
 const openNewsModal = async (newsId) => {
     const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
     try {
@@ -106,10 +118,11 @@ const openNewsModal = async (newsId) => {
     }
 }
 
+// Function to display news details
 const displayNewsModal = news => {
-    console.log(news)
     const modalTitle = document.getElementById('modal-title');
     modalTitle.innerText = news.title;
+
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
     <div class="d-flex justify-content-between gap-2">
@@ -125,12 +138,11 @@ const displayNewsModal = news => {
     `
     const modalRating = document.getElementById('modal-rating');
     modalRating.innerHTML = `
-    <p class="mb-0"><span class="fw-bold">Rating:</span> ${news.rating.number ? news.rating.number : 'No rating found'} <span class="badge bg-primary ms-2">${news.rating.badge ? news.rating.badge : 'No badge'}</span>
-</p>
+    <p class="mb-0"><span class="fw-bold">Rating:</span> ${news.rating.number ? news.rating.number : 'No rating found'} <span class="badge bg-primary ms-2">${news.rating.badge ? news.rating.badge : 'No badge'}</span></p>
     `
 }
 
-
+// Function to display loader
 const displayLoader = isTrue => {
     const newsLoader = document.getElementById('news-loader');
     if (isTrue) {
@@ -139,3 +151,14 @@ const displayLoader = isTrue => {
         newsLoader.classList.add('d-none');
     }
 }
+
+// Function to show active category
+document.getElementById('category-nav').addEventListener('click', function (e) {
+    const catLinks = document.querySelectorAll('.category-link');
+    for (link of catLinks) {
+        link.classList.remove('active')
+    }
+    if (e.target.classList.contains('category-link')) {
+        e.target.classList.add('active');
+    }
+})
